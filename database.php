3d4b091d071dc27f9b8315003cd977d4d5fdc797
Mohir <mohirkarimov4181@gmail.com>
2023-06-12 16:46:10 +0500

    full project

---
+++ database.php
@@ -0,0 +1,29 @@
+<?php  
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "php_blog";
+
+try {
+  $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  // set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
